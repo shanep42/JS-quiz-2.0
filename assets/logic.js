d54e89530bfcb4c90c6ebaff7a14f3db90505dd1
Shane <shanep42@gmail.com>
2022-08-07 19:48:50 -0500

    Adds JS logic for detecting right answers

--- assets/logic.js
+++ assets/logic.js
@@ -1,10 +1,13 @@
 var startButton = document.getElementById('start-button')
 startButton.addEventListener('click', setTime);
-startButton.addEventListener('click', timedQuiz);
+startButton.addEventListener('click', quiz);
 var timerEl = document.getElementById('time-display');
 var displayedQuestion = document.getElementById('question-space');
 var multipleChoices = document.querySelectorAll('.choice');
+var currentQuestionIndex = -1;
 var currentQuestion;
+var rightAnswerScore = 0;
+var wrongAnswerScore = 0;
 
 // I think I will likely need to write questions stored either as or like objects, so that each is accompanied by its multiple options for choices
 var questions = [{
@@ -24,6 +27,19 @@ var questions = [{
 //seconds to play the game, starting high for testing purposes
 var secondsLeft = 180;
 
+// TODO: Shuffle the questions so they are in a different order when you play more than once. 
+
+// function shuffle(array) {
+//   var m = array.length, t, i;
+//   while (m) {
+//     i = Math.floor(Math.random() * m--);
+//     t = array[m];
+//     array[m] = array[i];
+//     array[i] = t;
+//   }
+//   return array;
+// }
+
 
 function setTime() {
     startButton.classList.add("hidden")
@@ -33,24 +49,34 @@ function setTime() {
   
       if(secondsLeft === 0) {
         clearInterval(timerInterval);
+        //end quiz: Out of time
         alert("Time is up")
+
       }
   
     }, 1000);
     
   }
 
-
-function timedQuiz() {
-    fetchQuestion();
-    function fetchQuestion() {
-        currentQuestion = questions[Math.floor(Math.random() * questions.length)]
-
-        displayedQuestion.textContent = currentQuestion.question;
-
-        for (i = 0; i < currentQuestion.choices.length; i++){
-            multipleChoices[i].textContent = currentQuestion.choices[i];
-            multipleChoices[i].classList.remove('hidden');
-    }
+function quiz() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex > questions.length){
+    //end quiz: Out of questions
+    return;
   }
+
+  currentQuestion = questions[currentQuestionIndex];
+  displayedQuestion.textContent = currentQuestion.question;
+  for (let i = 0; i < currentQuestion.choices.length; i++){
+    multipleChoices[i].textContent = currentQuestion.choices[i];
+    multipleChoices[i].classList.remove('hidden');
+  }
+
+  document.addEventListener('click', function(event){
+    //right answers
+    if (event.target.dataset.optionnumber == currentQuestion.correctAnswer){
+      rightAnswerScore++;
+      quiz();
+    }
+  })
 }
